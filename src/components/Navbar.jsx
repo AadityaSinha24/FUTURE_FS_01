bf70e1fff3cff0f9bda8e0a19a865072c656b979
AadityaSinha24 <adityasinha21024@gmail.com>
2026-07-09 03:24:14 +0530

    fix css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,7 +26,7 @@ function Navbar() {
     })
   return (
     <div>
-        <nav className= {cn("absolute w-full z-40 transition-all duration-300",
+        <nav className= {cn("fixed w-full z-40 transition-all duration-300",
         isScrolled ? "py-3 bg-background/80 backdrop-blur-md shadow-xs" : "py-5"
         )}>
 
